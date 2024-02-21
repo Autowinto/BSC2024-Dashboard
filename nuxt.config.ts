021395edc260274fb58c65555ac77256490516ff
Magnus Christian Larsen <happymansi@hotmail.com>
2024-02-21 10:00:36 +0100

    Added nuxt ui package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/test-utils/module'],
+  modules: ['@nuxt/test-utils/module', '@nuxt/ui'],
   devServer: {
     port: 8080,
   },
