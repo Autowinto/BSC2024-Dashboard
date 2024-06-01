999d5d8eef7c89645caa92597d8590a80eeb8767
Magnus Christian Larsen <happymansi@hotmail.com>
2024-06-01 15:42:33 +0200

    Update useApi.ts

--- composables/useApi.ts
+++ composables/useApi.ts
@@ -1,6 +1,7 @@
+import process from 'node:process'
 import axios from 'axios'
 
-const client = axios.create({ baseURL: 'http://localhost:3000' })
+const client = axios.create({ baseURL: process.env.API_URL || 'http://localhost:3000' })
 
 export default function useApi() {
   return client
